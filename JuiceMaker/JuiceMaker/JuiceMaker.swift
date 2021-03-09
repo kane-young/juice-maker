afc90926551971dc26b3ebba128718039853c7d8
blanche37 <wkddbsghk16@gmail.com>
2021-03-09 09:53:38 +0900

    step1

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -1,12 +1,62 @@
-struct JuiceMaker {
-    var strawberry: Int = 10
-    var banana: Int
-    var pineapple: Int
-    var kiwi: Int
-    var mango: Int
-    var fruitArray = [10, 10, 10, 10, 10]
-    func a() {
-        print(fruitArray[0], [1])
-    }
+import Foundation
+
+enum Juice {
+    case strawberryJuice, bananaJuice, pineappleJuice, kiwiJuice, mangoJuice, strawberrybananaJuice, mangoKiwiJuice
 }
+
+struct JuiceMaker {
+    
+    static private(set) var strawberry: UInt = 10
+    static private(set) var banana: UInt = 10
+    static private(set) var pineapple: UInt = 10
+    static private(set) var kiwi: UInt = 10
+    static private(set) var mango: UInt = 10
+    
+    var fruitsArray: [UInt] = [strawberry, banana, pineapple, kiwi, mango]
+    
+    
+    var juice: Juice = Juice.bananaJuice
+    
+    mutating func makeJuice() {
+        switch juice {
+        case .strawberryJuice:
+            fruitsArray[0] -= 16
+        case .bananaJuice:
+            fruitsArray[1] -= 2
+        case .pineappleJuice:
+            fruitsArray[2] -= 2
+        case .kiwiJuice:
+            fruitsArray[3] -= 3
+        case .mangoJuice:
+            fruitsArray[4] -= 3
+        case .strawberrybananaJuice:
+            fruitsArray[0] -= 10
+            fruitsArray[1] -= 1
+        case .mangoKiwiJuice:
+            fruitsArray[4] -= 2
+            fruitsArray[3] -= 1
+        }
+        print(fruitsArray)
+    }
+    mutating func plusStock() {
+        
+            switch fruitsArray {
+            case [0]:
+                fruitsArray[0] += 1
+            case [1]:
+                fruitsArray[1] += 1
+            case [2]:
+                fruitsArray[2] += 1
+            case [3]:
+                fruitsArray[3] += 1
+            default:
+                fruitsArray[4] += 1
+            }
+           
+        }
+   
+   
+}
+
+
 
